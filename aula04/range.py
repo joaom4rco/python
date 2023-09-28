811fddfac27206edf1d9aeec73912dba74654d0f
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-09-28 19:08:45 -0300

    .

--- aula04/range.py
+++ aula04/range.py
@@ -1,3 +1,10 @@
 minhaLista = list(range(10,-11,-1))
 print(minhaLista)
 
+contagem = list(range(0,100, 6))
+print(len(contagem))
+if 77 in contagem:
+    print('o número 77 faz parte da contagem')
+else:
+    print('o número 77 não faz parte')
+print(contagem)
