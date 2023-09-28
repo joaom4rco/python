d97f422821618bea18875c59d7178d6aa446513f
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-09-28 20:24:11 -0300

    .

--- aula05/python.py
+++ aula05/python.py
@@ -1,3 +1,8 @@
 meuNome = "'joao'"
-meuNome = meuNome.center(100)
+meuNome = meuNome.center(102)
 print(meuNome)
+meuNome = meuNome.rjust(150)
+print(meuNome)
+meuNome2 = "'joao'"
+meuNome2 = meuNome2.find('joao')
+print(meuNome2)
---
+++ aula05/python02.py
@@ -0,0 +1,7 @@
+meuNome = 'joao', 'guilherme'
+count = meuNome.count('guilherme')
+print(count)
+
+meunome2 = 'joao marco santos andrade'
+meunome2 = meunome2.split('o')
+print(meunome2)
