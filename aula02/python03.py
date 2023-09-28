60446c60330f3d5a14939c7862b8fb733f88b0c1
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-09-28 16:38:07 -0300

    .

---
+++ aula02/python03.py
@@ -0,0 +1,10 @@
+numeros = [1,2,3,4,5,6]
+if 3 in numeros: 
+    print("o numero 3 está na lista")
+else:
+    print('o número 3 não está na lista')
+
+if 'o' in 'banana':
+    print('o está presente na banana')
+else:
+    print('o nao está presente na banana')
