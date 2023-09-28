bb134cbd2a4bb240338648d346d05974b1ef9733
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-09-28 20:37:35 -0300

    .

---
+++ aula06/python.py
@@ -0,0 +1,8 @@
+lista = [2,5,88,55]
+somaDaLista = lista[0]= 5**2
+print(somaDaLista)
+
+lista2 = [2,55,8,87,4,45]
+somaDaLista2 = (((5+8)+(5*2))/5)//2
+resultado = (lista[2] + somaDaLista2) / 2
+print(resultado)
