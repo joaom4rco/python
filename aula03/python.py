6376d630b8f30fc6656614677580cf51b9332ca7
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-09-28 17:03:24 -0300

    .

---
+++ aula03/python.py
@@ -0,0 +1,9 @@
+lista = [1,5,6,8,7,9,4,]
+lista[4] = 150
+print(lista)
+
+
+lista2 = [1,5,8,7,5,6,]
+print(lista2)
+lista2[2] = 'luiz'
+print(lista2)
---
+++ aula03/python02.py
@@ -0,0 +1,3 @@
+lista = [2,8,9,66,55,77]
+novoNumero = lista.append(2000)
+print(lista)
