52e41d1cb2afd6d4e0d405b6cd216970a8d37115
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-09-28 16:34:12 -0300

    .

---
+++ aula02/python.py
@@ -0,0 +1,5 @@
+theSum = 2
+num = 5
+num2 = 7
+soma = (theSum + num) * num2 
+print(soma)
---
+++ aula02/python02.py
@@ -0,0 +1,9 @@
+minhaLista = [ 5, 8 , 'joao', 'marco', 8, 5, 6 ]
+minhaLista = minhaLista *2
+print(minhaLista)
+
+minhaLista = [ 5, 8 , 'joao', 'marco', 8, 5, 6 ],[ 8,4 ],['marcelo'],[ 8, 5, 6 ]
+minha = minhaLista[1:3]
+print(minha)
+minha = len(minhaLista)
+print(len(minhaLista))
