cfc2f3c3fd5953c9cbce414905d77dabc1bbe8f8
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-09-28 18:20:26 -0300

    .

--- aula03/python03.py
+++ aula03/python03.py
@@ -2,6 +2,10 @@ numero = ['lla','ddd']
 indice = numero.index('lla')
 print(indice)
 
-fruta = [8,88,55,66,54]
+fruta = [8,88,55,66,54, 8 , 54]
 indice2 = fruta.index(88)
 print(indice2)
+indice2 = fruta.count(54)
+print(indice2)
+fruta.remove(54)
+print(fruta)
